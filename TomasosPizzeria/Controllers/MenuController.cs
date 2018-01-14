2647a32d09546535a3e199f51d31f71ac893fa2f
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2018-01-14 14:36:35 +0100

    Second commit

---
+++ TomasosPizzeria/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TomasosPizzeria.Models;
+
+
+namespace TomasosPizzeria.Controllers
+{
+    public class MenuController : Controller
+    {
+        private IMatrattRepository repository;
+
+        public MenuController(IMatrattRepository repo) { repository = repo; }
+
+        public IActionResult ShowMenu()
+        {
+            var matratter = repository.Matratter;
+            return View(matratter);
+        }
+    }
+}
